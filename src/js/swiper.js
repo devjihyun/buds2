1be9f9613eb3bd59d379e470e0db69dd04c9b36b
devjihyun <wlgus614@gmail.com>
2021-11-22 07:45:54 +0900

    ddd

---
+++ src/js/swiper.js
@@ -0,0 +1,82 @@
+import Swiper from "./swiper-bundle";
+
+var swiper = new Swiper(
+    '.buy-swiper-container', {
+	direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    a11y: {
+        prevSlideMessage: "이전 슬라이드로",
+        nextSlideMessage: "다음 슬라이드로",
+        paginationBulletMessage: "{{index}}번 슬라이드로 이동",
+        slideLabelMessage: "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드",
+    },
+
+    on: {
+        slideChangeTransitionStart: function() {
+            $(".swiper-pagination .swiper-pagination-bullet")
+            .attr("aria-current", "false")
+            .filter(".swiper-pagination-bullet-active")
+            .attr("aria-current", "true")
+        }
+    }
+});
+
+// var menu = ['Slide 1', 'Slide 2', 'Slide 3', 'Slide 4']
+
+var swiper = new Swiper(
+    '.pod-swiper-container', {
+	direction: 'horizontal',
+    loop: true,
+
+    pagination: {
+        // el: '.swiper-pagination',
+        // clickable: true,
+        // renderBullet: function (index, className) {
+        // return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        // }
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    a11y: {
+        prevSlideMessage: "이전 슬라이드로",
+        nextSlideMessage: "다음 슬라이드로",
+        paginationBulletMessage: "{{index}}번 슬라이드로 이동",
+        slideLabelMessage: "총 {{slidesLength}}장의 슬라이드 중 {{index}}번 슬라이드",
+    },
+
+    on: {
+        slideChangeTransitionStart: function() {
+            $(".swiper-pagination .swiper-pagination-bullet")
+            .attr("aria-current", "false")
+            .filter(".swiper-pagination-bullet-active")
+            .attr("aria-current", "true")
+        }
+    }
+});
+
+
+
+var swiper = new Swiper(
+    '.others-swiper-container', {
+	direction: 'horizontal',
+    slidesPerView: "auto",
+
+    a11y: {
+        slideLabelMessage: null,
+    },
+});
